377cccc51204e36ec4b81637463f9e2ee2d09918
gabrielsilper <gabrielsilper.main@gmail.com>
2026-02-02 19:24:02 -0400

    feat: entidade Produto

---
+++ src/entities/produto.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import Categoria from './categoria';
+
+@Entity('produtos')
+export default class Produto {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  nome!: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  descricao?: string;
+
+  @Column({ type: 'decimal', nullable: false })
+  preco!: number;
+
+  @Column({ type: 'int', nullable: false })
+  estoque?: number;
+
+  @CreateDateColumn({ name: 'data_criacao' })
+  dataCriacao!: Date;
+
+  @UpdateDateColumn({ name: 'data_atualizacao' })
+  dataAtualizacao!: Date;
+
+  @ManyToOne(() => Categoria, { onDelete: 'NO ACTION' })
+  @JoinColumn({ name: 'categoria_id' })
+  categoria!: Categoria;
+}
