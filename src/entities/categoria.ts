8dda7dfed1951ebb0f49f161343479e00f018ad7
lorena_silva <lorenacosta160400@gmail.com>
2026-02-02 19:09:49 -0400

    Entidade categoria

---
+++ src/entities/categoria.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export default class Categoria{
+    @PrimaryGeneratedColumn("uuid")
+     id!: string;
+
+    @Column({ type: "varchar", nullable: false })
+    nome!: string;
+
+    @Column({ type: "varchar", nullable: true })
+    descricao?: string;
+
+    @CreateDateColumn ({ type: "date", nullable: false })
+    dataCriacao!: Date;
+
+    @UpdateDateColumn ({ type: "date", nullable: false })
+    dataAtualizacao!: Date;
+
+}
