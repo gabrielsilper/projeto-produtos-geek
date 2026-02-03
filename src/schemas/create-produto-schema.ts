81c7457afc3cf6a85d9a385b8825bca677634e62
gabrielsilper <gabrielsilper.main@gmail.com>
2026-02-02 20:56:41 -0400

    feat: schema de produto finalizado

--- src/schemas/create-produto-schema.ts
+++ src/schemas/create-produto-schema.ts
@@ -1,3 +1,28 @@
 import { z } from 'zod';
 
-export const createSensorSchema = z.object({});
+export const createProdutoSchema = z.object({
+  nome: z
+    .string('Nome é obrigatório e deve ser uma string.')
+    .min(3, 'Nome deve ter pelo menos 3 caracteres.')
+    .max(50, 'Nome deve ter no máximo 50 caracteres.')
+    .regex(/^[a-zA-Z\s]+$/, 'Nome só pode conter letras e espaços.'),
+
+  descricao: z
+    .string('Descrição é opcional, mas deve ser uma string.')
+    .min(3, 'Descrição deve ter pelo menos 3 caracteres.')
+    .max(120, 'Descrição deve ter no máximo 120 caracteres.')
+    .regex(/^[a-zA-Z\s]+$/, 'Descrição só pode conter letras e espaços.')
+    .optional(),
+
+  preco: z
+    .number('Preço é um valor real (ex. 12.50) e é obrigatório.')
+    .refine((preco) => preco > 0, 'Preço deve ter maior que zero!'),
+
+  estoque: z
+    .int('Estoque é um valor inteiro e é obrigatório.')
+    .gte(0, 'Estoque deve ser maior ou igual a zero!'),
+
+  categoria: z
+    .int('Você deve informar obrigatoriamente um ID de uma categoria')
+    .gte(0, 'O ID da categoria deve ser maior ou igual a zero!'),
+});
