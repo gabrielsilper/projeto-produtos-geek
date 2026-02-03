aa4ef17dc4aee3a1dc6774d744b915064be16111
gabrielsilper <gabrielsilper.main@gmail.com>
2026-02-02 21:04:52 -0400

    feat: validação de produto

--- src/routes/produto.routes.ts
+++ src/routes/produto.routes.ts
@@ -1,6 +1,8 @@
 import ProdutoController from 'controllers/produto-controller';
 import { Router } from 'express';
+import { validateBody } from 'middlewares/validate-body';
 import ProdutoRepository from 'repositories/produto-repository';
+import { createProdutoSchema } from 'schemas/create-produto-schema';
 import ProdutoService from 'services/produto-service';
 
 const produtoRouter = Router();
@@ -8,7 +10,7 @@ const produtoService = new ProdutoService(new ProdutoRepository());
 const produtoController = new ProdutoController(produtoService);
 
 produtoRouter.get('/', (req, res) => produtoController.getAll(req, res));
-produtoRouter.post('/', (req, res) => produtoController.create(req, res));
+produtoRouter.post('/', validateBody(createProdutoSchema), (req, res) => produtoController.create(req, res));
 produtoRouter.get('/:id', (req, res) => produtoController.getById(req, res));
 produtoRouter.put('/:id', (req, res) => produtoController.update(req, res));
 produtoRouter.delete('/:id', (req, res) => produtoController.delete(req, res));
