f4c4190e9341836b9b634e37f57d6cf3f7b5ffda
gabrielsilper <gabrielsilper.main@gmail.com>
2026-02-02 20:17:59 -0400

    refactor: ajusta nomes no controller do produto

--- src/controllers/produto-controller.ts
+++ src/controllers/produto-controller.ts
@@ -6,27 +6,27 @@ export default class ProdutoController {
   constructor(private produtoService: ProdutoService) {}
 
   async getAll(_req: Request, res: Response) {
-    const sensors = await this.produtoService.getAll();
-    return res.json(sensors);
+    const produto = await this.produtoService.getAll();
+    return res.json(produto);
   }
 
   async getById(req: Request, res: Response) {
     const { id } = req.params;
-    const sensor = await this.produtoService.getById(id as string);
-    return res.json(sensor);
+    const produto = await this.produtoService.getById(id as string);
+    return res.json(produto);
   }
 
   async create(req: Request, res: Response) {
-    const sensorData = req.body as Produto;
-    const newSensor = await this.produtoService.create(sensorData);
-    return res.status(201).json(newSensor);
+    const produtoData = req.body as Produto;
+    const newProduto = await this.produtoService.create(produtoData);
+    return res.status(201).json(newProduto);
   }
 
   async update(req: Request, res: Response) {
     const { id } = req.params;
-    const sensorData = req.body as Partial<Produto>;
-    const updatedSensor = await this.produtoService.update(id as string, sensorData);
-    return res.json(updatedSensor);
+    const produtoData = req.body as Partial<Produto>;
+    const updatedProduto = await this.produtoService.update(id as string, produtoData);
+    return res.json(updatedProduto);
   }
 
   async delete(req: Request, res: Response) {
