f557864b35c52ac3944864a171d602885c9b454a
lorena_silva <lorenacosta160400@gmail.com>
2026-02-02 20:50:04 -0400

    validação zod - categoria

--- src/schemas/create-categoria-schema.ts
+++ src/schemas/create-categoria-schema.ts
@@ -1,3 +1,18 @@
-import { z } from 'zod';
+import { regex, z } from 'zod';
 
-export const createCategoriaSchema = z.object({});
+export const createCategoriaSchema = z.object({
+    nome:z
+    .string("Nome é obrigatório, deve ser único e uma string")
+    .min(3, "Nome deve ter pelo menos 3 caracteres")
+    .max(50, "Nome deve ter no máximo 50 caracteres")
+    .regex(/^[a-zA-Z\s]+$/,"Nome pode conter apenas letras e espaços"),
+
+    descricao:z
+    .string("Descrição é opcional e uma string")
+    .min(3, "Descrição deve ter pelo menos 3 caracteres")
+    .max(50, "Descriçãor no máximo 50 caracteres")
+    .regex(/^[a-zA-Z\s]+$/,"Descrição conter só letra e espaços")
+    .optional(),
+});
+
+
