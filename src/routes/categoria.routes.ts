6ddeaad750b3a412d6e30b8df0c10372d8772f2f
lorena_silva <lorenacosta160400@gmail.com>
2026-02-02 21:04:28 -0400

    validação na rota categoria

--- src/routes/categoria.routes.ts
+++ src/routes/categoria.routes.ts
@@ -1,6 +1,8 @@
 import CategoriaController from "controllers/categoria-controller";
 import { Router } from "express";
+import { validateBody } from "middlewares/validate-body";
 import CategoriaRepository from "repositories/categoria-repository";
+import { createCategoriaSchema } from "schemas/create-categoria-schema";
 import CategoriaService from "services/categoriaService";
 
 const categoriaRouter = Router();
@@ -9,7 +11,7 @@ const categoriaController = new CategoriaController(categoriaService)
 
 
 categoriaRouter.get('/',(req,res)=> categoriaController.getAll(req,res));
-categoriaRouter.post('/', (req, res) => categoriaController.create(req, res));
+categoriaRouter.post('/', validateBody (createCategoriaSchema), (req, res) => categoriaController.create(req, res));
 categoriaRouter.get('/:id', (req, res) => categoriaController.getById(req, res));
 categoriaRouter.put('/:id', (req, res) => categoriaController.update(req, res));
 categoriaRouter.delete('/:id', (req, res) => categoriaController.delete(req, res));
